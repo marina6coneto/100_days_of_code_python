216a96eee666fc36edfd8f9b4d827003cbed498b
marina6coneto <santoscmarina@hotmail.com>
2024-06-25 18:00:00 -0300

    hangman

--- day_7/hangman.py
+++ day_7/hangman.py
@@ -5,39 +5,40 @@ from hangman_arts import logo, stages
 print(logo)
 word = choice(word_list)
 word_length = len(word)
-display = []
+display = ['_' for _ in range(word_length)]
 end_of_the_game = False
 lives = 6
+guessed_letters = []  
 
-for i in range(word_length):
-    display += '_'
-print(f'{' '.join(display)}')
+print(' '.join(display))
 
 while not end_of_the_game:
 
-    guess = input('Guess a letter: ').lower()
-    
-    if guess in display:
-      print(f"You've already chosen this letter {guess}")
+  guess = input('Guess a letter: ').lower()
 
-    for position in range(word_length):
-        letter = word[position]
-        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-        if letter == guess:
-          display[position] = letter
-          
-    if guess not in word:
-      print(f"You guessed {guess}, that's not in the word. You lose a life.")
-      lives -= 1
-      if lives == 0:
-        end_of_the_game = True
-        print("GAME OVER. YOU'VE LOST! HAHAHAHA")
-          
-    print(f'{' '.join(display)}')
-           
-    if '_' not in display:
-        end_of_the_game = True
-        print('You win!')
-        
-    print(stages[lives])
-        
+  if guess in guessed_letters:
+    print(f"You've already chosen the letter '{guess}'. Try again.")
+    continue
+
+  guessed_letters.append(guess)
+
+  if guess not in word:
+    print(f"You guessed '{guess}', that's not in the word. You lose a life.")
+    lives -= 1
+    if lives == 0:
+      end_of_the_game = True
+      print(f"GAME OVER. YOU'VE LOST! The word was '{word}'.")
+      break
+
+  for position in range(word_length):
+    letter = word[position]
+    if letter == guess:
+      display[position] = letter
+
+  print(' '.join(display))
+
+  if '_' not in display:
+    end_of_the_game = True
+    print('CONGRATULATIONS! You win!')
+
+  print(stages[lives])
