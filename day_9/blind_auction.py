0340d6b4a721b8de61858e08265dbdfe6ba03b15
marina6coneto <santoscmarina@hotmail.com>
2024-06-27 22:59:36 -0300

    blind auction

--- day_9/blind_auction.py
+++ day_9/blind_auction.py
@@ -7,7 +7,7 @@ def clear():
 print(logo)
 
 bids = {}
-bidding_started = True
+bidding_finished = False
 
 def find_highest_bidder(bidding_record):
     highest_bid = 0
@@ -15,16 +15,15 @@ def find_highest_bidder(bidding_record):
         bid_amount = bidding_record[bidder]
         if bid_amount > highest_bid:
             highest_bid = bid_amount
-            winner = bidder
-    print(f'The winner is {winner} with a bid of ${highest_bid}')
+    print(f'The winner is {bidder} with a bid of ${highest_bid}')
     
-while bidding_started:
+while not bidding_finished:
     name = input('What is your name?\n ')
     price = int(input('What is your bid? $\n'))
     bids[name] = price
     should_continue = input('Are there any other bidders? type "yes" or "no" ').strip().lower()
     if should_continue == 'no':
-        bidding_starded = False
+        bidding_finished = True
         find_highest_bidder(bids)
     elif should_continue == 'yes':
         clear()
