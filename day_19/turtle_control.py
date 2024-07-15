4936ba921a0e277ae7ce0fd9daca9f12e64d4625
marina6coneto <santoscmarina@hotmail.com>
2024-07-15 16:08:10 -0300

    Add turtle control script with movement and reset functionalities

---
+++ day_19/turtle_control.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+    
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+    
+def move_backwards():
+    tim.backward(10)
+    
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+    
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+    
+    
+screen.listen()
+screen.onkey(move_forwards, 'w')
+screen.onkey(turn_left, 'a')
+screen.onkey(move_backwards, 's')
+screen.onkey(turn_right, 'd')
+screen.onkey(clear, 'c')
+
+screen.exitonclick()
