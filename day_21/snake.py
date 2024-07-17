d3a28861fa2c3c75100094b18504b6923ac3fb57
marina6coneto <santoscmarina@hotmail.com>
2024-07-17 17:27:52 -0300

    Final Snake Game!

--- day_21/snake.py
+++ day_21/snake.py
@@ -20,7 +20,7 @@ class Snake():
             
     def add_segment(self, position):
         new_segment = Turtle('square')
-        new_segment.color('white')
+        new_segment.color('green')
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
