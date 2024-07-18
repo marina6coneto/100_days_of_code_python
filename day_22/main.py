d4223c13c3047941ca59caa9b150e9e70fd11bc7
marina6coneto <santoscmarina@hotmail.com>
2024-07-18 18:25:14 -0300

    pong game

---
+++ day_22/main.py
@@ -0,0 +1,50 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from time import sleep
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(800, 600)
+screen.title('My pong game')
+screen.tracer(0)
+
+r_paddle = Paddle((373,0))
+l_paddle = Paddle((-380,0))
+ball = Ball()
+scoreboard = Scoreboard()
+    
+screen.listen()
+screen.onkeypress(r_paddle.up, 'Up')
+screen.onkeypress(r_paddle.down, 'Down')
+screen.onkeypress(l_paddle.up, 'w')
+screen.onkeypress(l_paddle.down, 's')
+
+game_is_on = True
+
+while True:
+    sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    
+    #detect the collision with the wall 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+        
+    #detect collision with r_paddle 
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -350:
+        ball.bounce_x()
+        
+    #detect when r paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+        
+    #detect when l paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+screen.exitonclick()
