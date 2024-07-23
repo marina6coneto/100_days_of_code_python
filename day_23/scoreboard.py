6ebee66d1f7ed8a41b40c85e5d2e33e0443e8d9f
marina6coneto <santoscmarina@hotmail.com>
2024-07-23 17:26:02 -0300

    class

---
+++ day_23/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+FONT = ("Courier", 14, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-290, 275)
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f'Level: {self.level}', align='left', font=FONT)
+        
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+        
+    def game_over(self):
+        self.goto(0,0)
+        self.write('GAME OVER ', align='center', font=FONT)
