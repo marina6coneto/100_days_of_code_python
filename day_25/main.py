18bd8ada380021d5cddfe66c266b9583eeb7823f
marina6coneto <santoscmarina@hotmail.com>
2024-07-29 17:02:04 -0300

    us states game

--- day_25/main.py
+++ day_25/main.py
@@ -15,13 +15,11 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f'{len(guessed_states)}/50 States Correct',
                                     prompt="What's another State's name?").title()
     if answer_state == 'Exit':
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv('day_25/states_to_learn.csv')
-        break
+        break    
+        
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
