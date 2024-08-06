22330437158de977e12b92c544a9ce3211661fe0
marina6coneto <santoscmarina@hotmail.com>
2024-08-06 18:01:44 -0300

    update main.py

--- day_29/main.py
+++ day_29/main.py
@@ -2,11 +2,14 @@ from tkinter import Tk, Canvas, PhotoImage, Button, Label, Entry, END, messagebo
 from random import choice, randint, shuffle
 import pyperclip
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# -------------------------- PASSWORD GENERATOR ----------------------------- #
 
 def generator_password():
     
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
