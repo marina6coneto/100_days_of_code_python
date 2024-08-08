7343f9e89c31952fa98a0375bd1d95dad52585d4
marina6coneto <santoscmarina@hotmail.com>
2024-08-08 09:06:27 -0300

    try and except update

--- day_26/main.py
+++ day_26/main.py
@@ -4,9 +4,17 @@ data = read_csv('day_26/nato_phonetic_alphabet.csv')
 
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-word = input('Enter a word: ').strip().upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input('Enter a word: ').strip().upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print('Sorry, only letter in the alphabet please.')
+        generate_phonetic()
+    else:
+        print(output_list)
+        
+generate_phonetic()
 
 
 
