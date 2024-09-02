f23134d46b5ef1cca2c5e6520cf58fc826701c72
marina6coneto <santoscmarina@hotmail.com>
2024-09-02 15:30:20 -0300

    question data

---
+++ day_34/data.py
@@ -0,0 +1,13 @@
+import requests
+
+parameters = {
+    'amount': 10,
+    'type': 'boolean',
+    'category': 18
+}
+
+response =  requests.get('https://opentdb.com/api.php', params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data = data['results']
+
