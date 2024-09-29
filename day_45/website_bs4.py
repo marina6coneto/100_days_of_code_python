0613b8514a51a6f9aa16a4fe43e65df5abe0d398
marina6coneto <santoscmarina@hotmail.com>
2024-09-29 11:15:07 -0300

    learning about bs4

---
+++ day_45/website_bs4.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get('https://news.ycombinator.com/news')
+
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, 'html.parser')
+article_tags = soup.find_all(name='a')
+article_texts = []
+article_links = []
+for article_tag in article_tags:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get('href')
+    article_links.append(link)    
+    
+      
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name='span', class_='score')]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+print(article_texts[largest_index])
+print(article_links[largest_index])
+
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
+
+
