fb105d5017334e472f2ff888d683ef94b2023218
marina6coneto <santoscmarina@hotmail.com>
2024-10-03 17:20:11 -0300

    main file

--- day_47/main.py
+++ day_47/main.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 url = '''https://www.amazon.com.br/Funko-Harry-Potter-Pop/dp/B076HVJN7T/ref=sr_1_3?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&sr=8-3&ufe=app_do%3Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9'''
 
-
 header = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Encoding": "gzip, deflate, br, zstd",
@@ -25,27 +24,9 @@ header = {
 }
 
 response = requests.get(url, headers=header)
-
 soup = BeautifulSoup(response.text, 'html.parser')
-
-
 price = soup.find('span', class_='a-offscreen').get_text()
-
 price_without_currency = price.replace('R$', '').replace(',', '.').strip()
-
-price_as_float = float(price_without_currency)
-
-# ---------------------------- SEND EMAIL ------------------------------- #
-
-title = soup.find(id = 'productTitle').get_text().strip()
- 
-BUY_PRICE = 140
-
-if price_as_float < BUY_PRICE:
-    message = f"{title} is on sale for {price_as_float}! Buy it now!"
-    
-    with smtplib.SMTP(os.environ['SMTP_ADDRESS'], port=587) as connection:
-        connection.starttls()
         result = connection.login(os.environ['EMAIL_ADDRESS'], os.environ['EMAIL_PASSWORD'])
         connection.sendmail(
             from_addr=os.environ['EMAIL_ADDRESS'],
